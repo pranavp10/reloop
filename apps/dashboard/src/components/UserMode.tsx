e4e15f9969d43f260d495dccf53aef8697851998
Pranav <pranavkp.me@outlook.com>
2025-05-07 19:16:40 +0530

    feat: enhance UserMode component to utilize changeMode function for routing

--- apps/dashboard/src/components/UserMode.tsx
+++ apps/dashboard/src/components/UserMode.tsx
@@ -3,26 +3,17 @@
 import { usePush } from "@/hooks/usePush";
 import { RadioGroup, RadioGroupItem } from "@reloop/ui/components/radio-group";
 import { Terminal, Workflow } from "lucide-react";
-import { useRouter } from "next/navigation";
 import { useId } from "react";
 
 export default function UserMode() {
-  const router = useRouter();
   const id = useId();
-  const { mode } = usePush();
+  const { mode, changeMode } = usePush();
 
   return (
     <div className="bg-input/50 inline-flex h-9 rounded-full p-0.5 ">
       <RadioGroup
         value={mode}
-        onValueChange={(value) => {
-          if (value === "dev") {
-            router.push("/dev");
-          }
-          if (value === "marketing") {
-            router.push("/marketing");
-          }
-        }}
+        onValueChange={changeMode}
         className="flex-1 group after:bg-background has-focus-visible:after:border-ring has-focus-visible:after:ring-ring/50 relative inline-grid grid-cols-[1fr_1fr] items-center gap-0 text-sm font-medium after:absolute after:inset-y-0 after:w-1/2 after:rounded-full after:shadow-xs after:transition-[translate,box-shadow] after:duration-300 after:ease-[cubic-bezier(0.16,1,0.3,1)] has-focus-visible:after:ring-[3px] data-[state=dev]:after:translate-x-0 data-[state=marketing]:after:translate-x-full"
         data-state={mode}
       >
